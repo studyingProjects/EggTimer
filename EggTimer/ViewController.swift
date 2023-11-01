eb56256cec5cedde24c8f263e1d476e4064813e7
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2023-11-01 11:39:16 +0200

    Conditionals, Dict, Optionals

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTime = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let result = eggTime[hardness]!
+    }
+    
+    
 }
